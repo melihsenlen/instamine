a5988d0feca6d633a2acf25497a8c6fc0a35ebe4
melihsenlen <melihsenlen20@gmail.com>
2026-05-11 15:47:29 +0200

    Config menu adjustments - 1.2.2

--- src/main/java/com/instamine/InstamineClient.java
+++ src/main/java/com/instamine/InstamineClient.java
@@ -7,6 +7,7 @@ import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.resources.Identifier;
 
 import java.util.ArrayList;
 
@@ -26,38 +27,40 @@ public class InstamineClient implements ModMenuApi, ClientModInitializer {
             ConfigCategory category = builder.getOrCreateCategory(
                 net.minecraft.network.chat.Component.literal("Blocks")
             );
+            category.setCategoryBackground(Identifier.parse("minecraft:textures/block/deepslate.png"));
 
             ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
-        category.addEntry(
-            entryBuilder.startFloatField(
-                net.minecraft.network.chat.Component.literal("Target Hardness"),
-                Instamine.hardness
-            )
-            .setDefaultValue(1.5f)
-            .setMin(0.1f)
-            .setMax(50.0f)
-            .setTooltip(net.minecraft.network.chat.Component.literal("Minecraft defaults: Stone = 1.5, Cobblestone = 2.0, Deepslate = 3.0"))
-            .setSaveConsumer(value -> {
-                Instamine.hardness = value;
-                Instamine.saveConfig(FabricLoader.getInstance().getConfigDir(), Instamine.blocks);
-            })
-            .build()
-        );
+            category.addEntry(
+                entryBuilder.startFloatField(
+                    net.minecraft.network.chat.Component.literal("Target Hardness"),
+                    Instamine.hardness
+                )
+                .setDefaultValue(1.5f)
+                .setMin(0.1f)
+                .setMax(100f)
+                .setTooltip(net.minecraft.network.chat.Component.literal("Stone: 1.5, Cobblestone: 2.0, Deepslate: 3.0, Obsidian: 50"))
+                .setSaveConsumer(value -> {
+                    Instamine.hardness = value;
+                    Instamine.saveConfig(FabricLoader.getInstance().getConfigDir(), Instamine.blocks);
+                })
+                .build()
+            );
 
-        category.addEntry(
-            entryBuilder.startStrList(
-                net.minecraft.network.chat.Component.literal("Affected Blocks"),
-                new ArrayList<>(Instamine.blocks)
-            )
-            .setDefaultValue(new ArrayList<>(Instamine.DEFAULTS))
-            .setTooltip(net.minecraft.network.chat.Component.literal("Blocks that will mine at target hardness"))
-            .setInsertInFront(true)
-            .setSaveConsumer(newList -> Instamine.saveConfig(
-                FabricLoader.getInstance().getConfigDir(), newList
-            ))
-            .build()
-        );
+            category.addEntry(
+                entryBuilder.startStrList(
+                    net.minecraft.network.chat.Component.literal("Affected Blocks"),
+                    new ArrayList<>(Instamine.blocks)
+                )
+                .setDefaultValue(new ArrayList<>(Instamine.DEFAULTS))
+                .setTooltip(net.minecraft.network.chat.Component.literal("Provide block ID"))
+                .setInsertInFront(true)
+                .setExpanded(true)
+                .setSaveConsumer(newList -> Instamine.saveConfig(
+                    FabricLoader.getInstance().getConfigDir(), newList
+                ))
+                .build()
+            );
             return builder.build();
         };
     }
